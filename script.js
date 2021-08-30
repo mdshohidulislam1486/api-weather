2785ac9672c971c5e9718beaf2c7b411970f8cdc
mdshohidulislam1486 <74629151+mdshohidulislam1486@users.noreply.github.com>
2021-08-30 18:28:32 +0600

    weather is working but no city name suggestion found

--- script.js
+++ script.js
@@ -7,15 +7,46 @@ const serchByCityNmae = () =>{
     url = (`https://api.openweathermap.org/data/2.5/weather?q=${searchValue},${searchValue},${searchValue}&appid=22b1c6b38ac2a4ba41cfc34612e67570
 
     `)
-
     fetch(url)
     .then(res => res.json())
     .then(data => dispalyWeather(data))
 }
 
+// kelvin convert function 
+
+const temConvert=(kelvin)=>{
+  const temp=((kelvin- 273.15) * 9/5) + 32
+  var fToCel = (temp - 32) * 5/9;
+ var result = (fToCel).toFixed(2) + '\xB0C.';
+  
+  return result;
+}
+
 const dispalyWeather= cities =>{
+const getMainDipaly = document.getElementById('current-weather');
+getMainDipaly.innerHTML = '';
 const cityName = cities.name;
-console.log(cityName)
-const felxboxInner = document.getElementById('current-weather');
-console.log(cities.main.feels_like)
+const getCountry = cities.sys.country
+
+const getTemp = cities.main.temp;
+const tempe = temConvert(getTemp);
+
+const getFeelsLike  = cities.main.feels_like;
+const feellikes  = temConvert(getFeelsLike);
+
+const getWeathers =cities.weather
+for(const getWeatehr of getWeathers){
+  const weatherDes = getWeatehr.description
+  const weaterIcon = getWeatehr.icon
+  const getWeatehrMain = getWeatehr.main
+
+
+const div = document.createElement('div')
+div.innerHTML =`
+<h1 class='fw-900'> <span id="city">${cityName},</span> <span id="country">${getCountry}</span></h1>
+<h5>Feel like ${feellikes} ${getWeatehrMain}, ${weatherDes} </h5>
+<h2>${tempe}</h2>
+`
+getMainDipaly.appendChild(div)
+}
 }
